75a3e4403142809f00bb8eba9ef3000f5346fcc5
ee3509 <p_pelegris@hotmail.com>
2013-10-05 17:07:33 -0700

    part 3 author ?

--- HW.1/part3.rb
+++ HW.1/part3.rb
@@ -4,8 +4,6 @@ class BookInStock
   def initialize(isbn,price)
     
     raise ArgumentError, "Invalid ISBN" if(isbn.length<1 or price.to_i<1)
-    # raise ArgumentError, "Invalid Price" if(price.length<1)  
-    # raise ArgumentError, "Invalid Price" if(price.to_i<1)  
 
     @isbn=isbn
     @price=price
@@ -17,27 +15,6 @@ class BookInStock
   
 end
 
-book = BookInStock.new("2323213","25.40")
-book.isbn=12312312
-print book.price_as_string
-
-# book.price_as_string
-
-# On Time 
-# BookInStock
-  # should be defined
-  # getters and setters
-    # should set ISBN [10 points] (FAILED)
-    # should set price [10 points] (FAILED)
-    # should be able to change ISBN [10 points] (FAILED)
-    # should be able to change price [10 points] (FAILED)
-  # constructor
-    # should reject invalid ISBN number [10 points] (FAILED)
-    # should reject zero price [10 points] (FAILED)
-    # should reject negative price [10 points] (FAILED)
-  # #price_as_string
-    # should be defined (FAILED)
-    # should display 33.95 as "$33.95" [10 points] (FAILED)
-    # should display 1.1 as $1.10 [10 points] (FAILED)
-    # should display 20 as $20.00 [10 points] (FAILED)
-
+# book = BookInStock.new("2323213","25.40")
+# book.isbn=12312312
+# print book.price_as_string
