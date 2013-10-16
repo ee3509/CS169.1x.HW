f0dd866e926585dc4cc5142631aa77465b1635f7
ee3509 <p_pelegris@hotmail.com>
2013-10-15 22:35:45 -0700

    hw 1.5 all parts completed

--- HW.1.5/lib/oracle_of_bacon.rb
+++ HW.1.5/lib/oracle_of_bacon.rb
@@ -45,16 +45,18 @@ class OracleOfBacon
     rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
       Net::ProtocolError => e
-      # convert all of these into a generic OracleOfBacon::NetworkError,
-      #  but keep the original error message
-      # your code here
+      raise NetworkError.new(e.message)
     end
     # your code here: create the OracleOfBacon::Response object
+    res = Response.new(xml)
   end
 
   def make_uri_from_arguments
-    # your code here: set the @uri attribute to properly-escaped URI
-    #   constructed from the @from, @to, @api_key arguments
+    from = CGI.escape @from if @from != nil
+    to = CGI.escape @to if @to !=nil
+    api_key = CGI.escape @api_key if  @api_key != nil
+    
+    @uri = "http://oracleofbacon.org/cgi-bin/xml?p=#{api_key}&a=#{from}&b=#{to}"
   end
       
   class Response
@@ -70,10 +72,38 @@ class OracleOfBacon
     def parse_response
       if ! @doc.xpath('/error').empty?
         parse_error_response
-      # your code here: 'elsif' clauses to handle other responses
-      # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'         
+      elsif ! @doc.xpath('/spellcheck').empty?
+        parse_spellcheck_response
+      elsif ! @doc.xpath('/link').empty?
+        parse_graph_response
+      elsif ! @doc.xpath('/other').empty?
+        parse_unknown_response
       end
+    end
+    
+    def parse_unknown_response
+      @type = :unknown
+      @data = "unknown response type"
+    end
+    
+    def parse_spellcheck_response
+      @type = :spellcheck
+      @data = @doc.xpath("//match")
+      res = []
+      @data.each do |elem|
+        res << elem.text  
+      end
+      @data=res
+      
+    end
+    def parse_graph_response
+      @type = :graph
+      @data = @doc.xpath("//actor | //movie")
+      res = []
+      @data.each do |elem|
+        res << elem.text  
+      end
+      @data=res
     end
     def parse_error_response
       @type = :error
@@ -82,9 +112,9 @@ class OracleOfBacon
   end
 end
 
+# a = OracleOfBacon.new("38b99ce9ec87")
+# a.to = "Denise Richards"
+# res = a.find_connections
+# puts res.type
+# puts res.data
 
-# test = OracleOfBacon.new("123")
-# test.from="a"
-# test.to="a"
-# test.valid?
-# puts test.errors.full_messages
